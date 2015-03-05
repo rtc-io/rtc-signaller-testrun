0e1cb8b3885c45d6759a4a59c332a7b6bc8dd129
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 10:13:55 +1100

    gendocs

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+  # rtc-signaller-testrun
+
+  This is a suite of tests that will allow you to test how solid
+  your custom signaller is against some "real world" usage within
+  both [`rtc-quickconnect`](https://github.com/rtc-io/rtc-quickconnect)
+  and the lower level [`rtc-tools`](https://github.com/rtc-io/rtc-tools).
+
+  ## Example Usage
+
+  To be completed.
+**/
+module.exports = function(createSignaller, opts) {
+  // check the api signature first
+  // require('./modules/signaller/')(createSignaller, opts);
+};
