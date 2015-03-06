b0d95bc1709e8aa533cc27b1d6f52d275feb7873
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 14:40:16 +1100

    port request-stream test

--- modules/quickconnect/index.js
+++ modules/quickconnect/index.js
@@ -3,4 +3,5 @@ var detect = require('rtc-core/detect');
 module.exports = function(createSignaller, opts) {
   require('./profile')(createSignaller, opts);
   require('./datachannel')(createSignaller, opts);
+  require('./request-stream')(createSignaller, opts);
 };
