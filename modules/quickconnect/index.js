e00fb06cd6f51d95d8b37192cd04caab2847b7a7
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 14:21:20 +1100

    port quickconnect datachannel test

--- modules/quickconnect/index.js
+++ modules/quickconnect/index.js
@@ -2,4 +2,5 @@ var detect = require('rtc-core/detect');
 
 module.exports = function(createSignaller, opts) {
   require('./profile')(createSignaller, opts);
+  require('./datachannel')(createSignaller, opts);
 };
