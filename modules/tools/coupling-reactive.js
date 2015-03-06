8b9f424256573041126ccb3c71dc6a00f818f98c
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 12:28:25 +1100

    ported coupling-reactive test

---
+++ modules/tools/coupling-reactive.js
@@ -0,0 +1,14 @@
+module.exports = function(createSignaller, opts) {
+  var subtest = require('./subtest-reactive')(createSignaller, opts);
+  var stunGoog = require('./helpers/stun-google');
+
+  subtest('4 channels, no ice servers', { iceServers: [], channelCount: 4 });
+  subtest('4 channels, google stun servers', { iceServers: stunGoog, channelCount: 4 });
+
+  subtest('4 channels, no ice servers (delay 200 - 1000ms)', { iceServers: [], channelCount: 4, minDelay: 200, maxDelay: 500 });
+  subtest('4 channels, google stun servers (delay 200 - 1000ms)', { iceServers: stunGoog, channelCount: 4, minDelay: 200, maxDelay: 500 });
+
+  subtest('10 channels, no ice servers', { iceServers: [], channelCount: 10 });
+  subtest('10 channels, google stun servers', { iceServers: stunGoog, channelCount: 10 });
+};
+
