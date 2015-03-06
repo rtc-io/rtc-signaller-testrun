1ef7ac7f91387100f0878985774ae72ed5205336
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-06 12:22:14 +1100

    ported capture-close-localonly test

--- modules/tools/index.js
+++ modules/tools/index.js
@@ -1,4 +1,5 @@
 module.exports = function(createSignaller, opts) {
   require('./coupling')(createSignaller, opts);
   require('./coupling-constraints')(createSignaller, opts);
+  require('./capture-close-localonly')(createSignaller, opts);
 };
